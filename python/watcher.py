fbf86e44097d6cf4cc386d2a5e33a0959e7979cd
Eero af Heurlin <eero.afheurlin@iki.fi>
2015-06-16 21:03:36 +0300

    turn printer power off after timeout if there are no active jobs

--- python/watcher.py
+++ python/watcher.py
@@ -5,14 +5,13 @@ from __future__ import print_function
 import logging
 import yaml
 import json
+import time
 from tornado.ioloop import IOLoop, PeriodicCallback
 from tornado import gen
 from tornado.httpclient import AsyncHTTPClient
 
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
-
-
 
 LOGLEVEL = logging.DEBUG
 #LOGLEVEL = logging.INFO
@@ -21,6 +20,7 @@ class controller(object):
     config = {}
     pcbs = [] # List of active PeriodicCallback instances
     gpio_initialized = False
+    job_last_active = None
 
     def __init__(self, config_file, mainloop):
         self.config_file = config_file
@@ -73,19 +73,46 @@ class controller(object):
     def check_power(self):
         logging.debug("check_power called, current time %s" % (self.mainloop.time()))
         try:
-            pass
-#            response = yield AsyncHTTPClient().fetch(url, request_timeout=self.config['http_timeout'])
-#            if response.error:
-#                logging.warning("Got exception %s when fetching %s" % (response.error, url))
-#            else:
-#                logging.info("Fetched %s in %s seconds" % (url, response.request_time))
+            extra_headers = { 'X-Api-Key': self.config['API_KEY'] }
+            url = "%s/api/job" % self.config['API_BASE_URL']
+            response = yield AsyncHTTPClient().fetch(url, request_timeout=1, headers=extra_headers)
+            if response.error:
+                logging.warning("Got exception %s when fetching %s" % (response.error, url))
+            else:
+                logging.info("Fetched %s in %s seconds" % (url, response.request_time))
+                decoded = json.loads(response.body)
+                logging.debug("Decoded response %s" % repr(decoded))
+                if decoded['state'] == 'Printing':
+                    self.job_last_active = time.time()
+        except Exception, e:
+            logging.exception(e)
+        
+        if self.job_last_active:
+            if time.time() - self.job_last_active > self.config['MAIN_POWER_TIMEOUT']:
+                self.mainloop.spawn_callback(self.disable_printer_power)
+                
+            
+
+    @gen.coroutine
+    def enable_printer_power(self):
+        try:
+            logging.info("Enabling printer power")
+            self.job_last_active = time.time()
         except Exception, e:
             logging.exception(e)
         pass
-        
+
+    @gen.coroutine
+    def disable_printer_power(self):
+        try:
+            logging.info("Disabling printer power after %s seconds of inactivity" % self.config['MAIN_POWER_TIMEOUT'])
+        except Exception, e:
+            logging.exception(e)
+        pass
 
     def button_pressed(self, *args):
         logging.debug("Button pressed")
+        self.mainloop.spawn_callback(self.enable_printer_power)
         pass
 
 
